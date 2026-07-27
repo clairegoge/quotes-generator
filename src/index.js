20d9cb4d3fe0e726385801b4bb975ce621e48111
clairegoge <nonigoge@gmail.com>
2026-07-27 15:38:17 +0200

    added basic form handling and added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+function generateQuote(event) {
+  event.preventDefault();
+  alert("Generating quote");
 
+  new Typewriter("#quote", {
+    strings: [
+      "Life is really simple, but we insist on making it complicated. Confucius",
+    ],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let quoteFormElement = document.querySelector("#quote-generator-form");
+quoteFormElement.addEventListener("submit", generateQuote);
